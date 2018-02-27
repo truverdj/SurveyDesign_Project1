4764e6e6e4e54df38a969051cab987c4fcceddfc
Daniel Truver <MrTruver@Daniels-MacBook-Pro-2.local>
2018-02-27 13:56:32 -0500

    new file for contructing data set manually

---
+++ manual_data.R
@@ -0,0 +1,24 @@
+library(dplyr)
+load("sample.Rdata")
+sample.df$numtexts = rep(NA, nrow(sample.df))
+sample.df$newprice = rep(NA, nrow(sample.df))
+sample.df$usedprice = rep(NA, nrow(sample.df))
+n = nrow(sample.df)
+make_store_url = function(department, number){
+  url_part1 = "http://dukebooks.collegestoreonline.com/ePOS?wpd=1&width=100%25&this_category=1&term=SP18&store=320&step=5&qty=1000&listtype=begin&go=Go&form=shared3%2Ftextbooks%2Fno_jscript%2Fmain.html&design=duke_textbooks&department="
+  url_part2 = department
+  url_part3 = "&course="
+  url_part4 = as.character(number)
+  url_part5 = "&colspan=3&cellspacing=1&cellpadding=0&campus=MAIN&border=0&bgcolor=%23cccccc&action=list_courses&section=01&Go=Go"
+  store_url = paste(url_part1, url_part2, url_part3, url_part4, url_part5, sep = "")
+  return(store_url)
+}
+
+x = rep(NA, n)
+for (i in seq_along(x)){
+  x[i] = make_store_url(sample.df[i,"department"], sample.df[i, "course"])
+}
+
+sample.df$numtexts[(n/2 + 1):n] = c(0,1,0,0,0,1,)
+sample.df$newprice[(n/2 + 1):n] = c(0,253.50,0,0,0,117.50,)
+sample.df$usedprice[(n/2 +1):n] = c(0,190.25,0,0,0,88.25,)
